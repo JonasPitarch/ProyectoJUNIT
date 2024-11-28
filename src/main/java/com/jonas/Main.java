edd808e7cbdee9a9f5bf0b5b44b309f7637681c3
jonpitsaf <jonaspitarch06@gmail.com>
2024-11-28 09:47:26 +0100

    a

--- src/main/java/com/jonas/Main.java
+++ src/main/java/com/jonas/Main.java
@@ -4,6 +4,10 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import javax.naming.Context;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,5 +21,14 @@ public class Main {
         TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
 
+    }
+
+    public static void escriuhtml(String continguthtml, String nomFitxer){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(nomFitxer))) {
+            bw.write(continguthtml);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
