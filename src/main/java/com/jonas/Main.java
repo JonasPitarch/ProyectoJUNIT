c206ddbb2db696c4e5d0df76ae5b018b276af30f
jonpitsaf <jonaspitarch06@gmail.com>
2024-11-28 09:18:25 +0100

    a

--- src/main/java/com/jonas/Main.java
+++ src/main/java/com/jonas/Main.java
@@ -1,9 +1,22 @@
 package com.jonas;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import javax.naming.Context;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
 
     }
 }
